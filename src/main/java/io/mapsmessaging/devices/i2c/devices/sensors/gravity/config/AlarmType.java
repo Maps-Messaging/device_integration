21d18b9e910e01fef432e6960bf8380c4a945967
Matthew Buckton <matthew@buckton.org>
2023-07-06 09:25:14 +1000

    cleanup

--- src/main/java/io/mapsmessaging/devices/i2c/devices/sensors/gravity/config/AlarmType.java
+++ src/main/java/io/mapsmessaging/devices/i2c/devices/sensors/gravity/config/AlarmType.java
@@ -1,16 +1,16 @@
 package io.mapsmessaging.devices.i2c.devices.sensors.gravity.config;
 
+import lombok.Getter;
+
 public enum AlarmType {
   LOW_THRESHOLD((byte) 0x00),
   HIGH_THRESHOLD((byte) 0x01);
 
+  @Getter
   private final byte value;
 
   AlarmType(byte value) {
     this.value = value;
   }
 
-  public byte getValue() {
-    return value;
-  }
 }
--- src/main/java/io/mapsmessaging/devices/i2c/devices/sensors/gravity/config/Command.java
+++ src/main/java/io/mapsmessaging/devices/i2c/devices/sensors/gravity/config/Command.java
@@ -8,7 +8,7 @@ public enum Command {
   GET_TEMP(0x87, "Get the current sensor temperature"),
   GET_ALL_DATA(0x88, "Get all the data in one request"),
   SET_THRESHOLD_ALARMS(0x89, "Set threshold alarms high and low"),
-  I2C_DATA_AVAILABLE(0x90, "Is data avaliable to read"),
+  I2C_DATA_AVAILABLE(0x90, "Is data available to read"),
   SENSOR_VOLTAGE(0x91, "Read the raw ADC voltage sensor"),
   CHANGE_I2C_ADDR(0x92, "Change the devices I2C address, reboot is required after setting");
 
