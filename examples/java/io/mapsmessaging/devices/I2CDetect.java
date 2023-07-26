cf0cfa99b454d088a73ee845f67008de6c5da8e5
Matthew Buckton <matthew@buckton.org>
2023-07-26 20:28:55 +1000

    java version of I2CDetect

---
+++ examples/java/io/mapsmessaging/devices/I2CDetect.java
@@ -0,0 +1,37 @@
+/*
+ *      Copyright [ 2020 - 2023 ] [Matthew Buckton]
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ */
+
+package io.mapsmessaging.devices;
+
+import io.mapsmessaging.devices.i2c.I2CBusManager;
+
+import java.util.List;
+
+public class I2CDetect {
+
+  public static void main(String[] args) {
+    I2CBusManager[] i2CBusManagers = DeviceBusManager.getInstance().getI2cBusManager();
+    int bus = 1;
+    if (args.length > 0) {
+      bus = Integer.parseInt(args[0]);
+    }
+    List<Integer> list = i2CBusManagers[bus].findDevicesOnBus();
+    for (String line : i2CBusManagers[bus].listDetected(list)) {
+      System.err.println(line);
+    }
+  }
+
+}
