ae0bc45969965de1cea82e1acca46d005baf6320
Matthew Buckton <matthew@buckton.org>
2023-08-16 18:53:59 +1000

    simplify the function

--- src/main/java/io/mapsmessaging/devices/i2c/devices/output/TimeHelper.java
+++ src/main/java/io/mapsmessaging/devices/i2c/devices/output/TimeHelper.java
@@ -1,40 +1,19 @@
 package io.mapsmessaging.devices.i2c.devices.output;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeHelper {
 
-  public static String getTime(boolean hasColon, boolean addSeconds) {
-    StringBuilder val = new StringBuilder();
-    LocalTime dateTime = LocalTime.now();
-    int hour = dateTime.getHour();
-    int min = dateTime.getMinute();
-    int sec = dateTime.getSecond();
-    if (hour < 10) {
-      val.append("0");
-    }
-    val.append(hour);
+  public static String getTime(boolean useSpace, boolean addSeconds) {
+    // Define format patterns based on the parameters
+    String formatPattern = useSpace ? (addSeconds ? "HH mm ss" : "HH mm") : (addSeconds ? "HH:mm:ss" : "HH:mm");
 
-    if (hasColon) {
-      val.append(" ");
-    } else {
-      val.append(":");
-    }
-    if (min < 10) {
-      val.append("0");
-    }
-    val.append(min);
-    if (addSeconds) {
-      if (hasColon) {
-        val.append(" ");
-      } else {
-        val.append(":");
-      }
-      if (sec < 10) {
-        val.append("0");
-      }
-      val.append(sec);
-    }
-    return val.toString();
+    // Get the current time
+    LocalTime currentTime = LocalTime.now();
+
+    // Format and return the time string
+    return currentTime.format(DateTimeFormatter.ofPattern(formatPattern));
   }
+
 }
